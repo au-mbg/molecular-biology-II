af560f726acca29bb36ef96805f3c7513f930293
Mads-Peter <machri@phys.au.dk>
2026-06-10 13:57:32 +0200

    Decent version, not finished but decent.

--- course-utils/src/course_utils/check_answer.py
+++ course-utils/src/course_utils/check_answer.py
@@ -1,17 +1,22 @@
 from IPython.display import HTML, display
-import hashlib
 
 
-def check_answer(entered_code):
+def check_answer(answers: list[str]):
     # Final cell to check answer
-    correct_code = '5ef7327c49021d667b9333fae01f7ff3b9d62d83b325807c94bfdafaa14d9450'
 
-    hashed_code = hashlib.sha256(entered_code.strip().upper().encode()).hexdigest()
+    correct_answers = ['CAEDB', '!', '1', 'A', 'C', 'B', 'A']
+    correct_code = 'CAEDB!1ACBA'
+    answer = ''.join(answers)
 
     # Check if the entered code is correct
-    if hashed_code == correct_code:
+    if answer == correct_code:
         display(HTML("<h1 style='color: green;'>🎉 Correct! Great job! 🎉</h1>"))
         display(HTML("<img src='https://media.giphy.com/media/X9izlczKyCpmCSZu0l/giphy.gif' width='300'>"))  # Celebration GIF
     else:
+
+        for i, (a, c) in enumerate(zip(answers, correct_answers)):
+            if a != c:
+                display(HTML(f"<h2 style='color: orange;'>Question {i+1} is incorrect. Please review your answer.</h2>"))
+
         display(HTML("<h1 style='color: red;'>Incorrect. Try again!</h1>"))
         display(HTML("<img src='https://media.giphy.com/media/CoND5j6Bn1QZUgm1xX/giphy.gif' width='300'>"))
